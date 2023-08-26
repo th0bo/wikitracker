2c81d3e242c18bb6a308727f08a79f0c07c9f4b5
Théo Boulaire <th.boulaire@gmail.com>
2023-08-26 13:48:42 +0200

    First component

---
+++ components/distance.vue
@@ -0,0 +1,39 @@
+<script setup>
+defineProps(['current', 'target'])
+</script>
+
+<template>
+  <div>
+    <span>{{ current }}</span>
+    <span>{{ target }}</span>
+  </div>
+</template>
+
+<style scoped>
+div {
+  position: relative;
+  width: 14vh;
+  aspect-ratio: 1 / 1;
+  background-image: url(../assets/svg/score-frame.svg);
+  background-size: cover;
+}
+span {
+  position: absolute;
+  width: 10.5vh;
+  aspect-ratio: 1 / 1;
+  line-height: 10.5vh;
+  size: 4.2vh;
+  text-align: center;
+  font-weight: 600;
+}
+span:first-child {
+  top: 0;
+  left: 0;
+  color: var(--green);
+}
+span:last-child {
+  bottom: 0;
+  right: 0;
+  color: var(--red);
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <NuxtLink to="/">Home</NuxtLink>
+    <Distance target="8" current="5" width="14vw"></Distance>
     <slot />
     <NuxtLink to="/about">About</NuxtLink>
   </div>
