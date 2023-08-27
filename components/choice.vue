cf3d249c2a2d492fc792c5883910f8f0073384fc
Théo Boulaire <th.boulaire@gmail.com>
2023-08-27 21:03:38 +0200

    Line component

---
+++ components/choice.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps(['propertyName', 'propertyDisplay', 'itemId', 'itemDisplay']);
+</script>
+
+<template>
+  <div>
+    <div class="property" v-bind:aria-label="propertyName">{{ propertyDisplay }}</div>
+    <div class="item">{{ itemDisplay }}</div>
+  </div>
+</template>
+
+<style scoped>
+.property {
+  display: none;
+}
+</style>
---
+++ pages/game.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>Game</h1>
+  <Choice property-name="sous-classe de" property-display=" └─" item-id="Q333" item-display="Japon"></Choice>
+</template>
