9af50f146a9a95d9a7b03d416510a44a0256d77b
Théo Boulaire <th.boulaire@gmail.com>
2023-08-31 20:56:24 +0200

    Responsive choice style

--- components/choice.vue
+++ components/choice.vue
@@ -3,14 +3,27 @@ defineProps(['propertyName', 'propertyDisplay', 'itemId', 'itemDisplay']);
 </script>
 
 <template>
-  <div>
+  <div class="line">
     <div class="property" v-bind:aria-label="propertyName">{{ propertyDisplay }}</div>
     <div class="item">{{ itemDisplay }}</div>
   </div>
 </template>
 
 <style scoped>
+.line {
+  font-size: 20px;
+  display: grid;
+  grid-template-columns: 0 7fr;
+}
+
+@media (orientation: landscape) {
+  .line {
+    grid-template-columns: 5fr 7fr;
+    column-gap: 24px;
+  }
+}
+
 .property {
-  display: none;
+  text-align: right;
 }
 </style>
