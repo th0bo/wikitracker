e0a3436ed9bc67b293ae28f7bc45c25a5b9f1091
Théo Boulaire <th.boulaire@gmail.com>
2023-09-03 18:59:02 +0200

    Header and breadcrumb components

---
+++ components/breadcrumb.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav>
+    <ol>
+      <li>Soleil</li>
+      <li>(3959) Irwin</li>
+      <li>(3960) Chaliubieju</li>
+      <li class="etc">•••</li>
+      <li>feu</li>
+    </ol>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  font-size: 28px;
+}
+
+nav ol {
+  all: unset;
+  list-style: none;
+}
+
+nav ol li {
+  display: inline-block;
+  color: var(--blue);
+}
+
+nav ol li.etc {
+  color: var(--light);
+}
+
+nav ol li:first-child {
+  color: var(--green);
+}
+
+nav ol li:last-child {
+  color: var(--red);
+}
+
+nav ol li:not(:first-child)::before {
+  content: ">";
+  color: var(--light);
+  margin: 0 8px;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,8 @@
 <template>
   <div class="wrapper">
-    <header>
-      <Home></Home>
-      <Distance target="89" current="25"></Distance>
-      <Direction></Direction>
-    </header>
+    <Header></Header>
     <main>
-      <nav>
-        <ol>
-          <li>Soleil</li>
-          <li>(3959) Irwin</li>
-          <li>(3960) Chaliubieju</li>
-          <li class="etc">•••</li>
-          <li>feu</li>
-        </ol>
-      </nav>
+      <Breadcrumb></Breadcrumb>
       <slot />
     </main>
   </div>
@@ -23,10 +11,6 @@
 <style>
 body {
   margin: 0;
-}
-header > * {
-  display: inline-block;
-  width: fit-content;
 }
 
 .wrapper {
@@ -40,55 +24,10 @@ header > * {
   grid-template-rows: min-content auto;
 }
 
-header {
-  display: grid;
-  grid-template-columns: repeat(6, 1fr);
-  column-gap: inherit;
-}
-
 @media (orientation: landscape) {
   .wrapper {
     grid-template-columns: max-content auto;
     grid-template-rows: 100%;
   }
-
-  header {
-    grid-template-columns: 1fr;
-    grid-template-rows: repeat(6, 1fr);
-    column-gap: unset;
-    row-gap: inherit;
-  }
-}
-
-nav {
-  font-size: 28px;
-}
-
-nav ol {
-  all: unset;
-  list-style: none;
-}
-
-nav ol li {
-  display: inline-block;
-  color: var(--blue);
-}
-
-nav ol li.etc {
-  color: var(--light);
-}
-
-nav ol li:first-child {
-  color: var(--green);
-}
-
-nav ol li:last-child {
-  color: var(--red);
-}
-
-nav ol li:not(:first-child)::before {
-  content: ">";
-  color: var(--light);
-  margin: 0 8px;
 }
 </style>
