b5f40d72a87b46b58726f00b7079c8f3579a0a39
Théo Boulaire <th.boulaire@gmail.com>
2023-09-17 08:51:58 +0200

    Transition when options change

--- components/Game.vue
+++ components/Game.vue
@@ -57,7 +57,7 @@ const won = computed(() => current.value.id === endId);
 watch(current, (newCurrentValue, oldCurrentValue) => {
   const nextStep = steps.value[index.value];
   if (nextStep !== undefined) {
-    backward.value = !nextStep.forward; 
+    backward.value = !nextStep.forward;
   } else {
     backward.value = !current.value.forward;
   }
@@ -66,29 +66,15 @@ watch(current, (newCurrentValue, oldCurrentValue) => {
 </script>
 
 <template>
-  <EndModal
-    v-if="won"
-    :steps="steps"></EndModal>
-  <Header
-    @toggle-backward="backward = !backward"
-    :backward="backward"
-    ></Header>
+  <EndModal v-if="won" :steps="steps"></EndModal>
+  <Header @toggle-backward="backward = !backward" :backward="backward"></Header>
   <main>
-    <Breadcrumb
-      v-if="data"
-      @step-back="(i: number) => index = i"
-      :start="data.start"
-      :steps="steps"
-      :end="data.end"
-      :forward="!backward"
-      :key="[current.forward, data.start, ...steps, data.end].join('_')"
-      ></Breadcrumb>
-    <Options
-      @step-advance="(step: Step) => { steps = [...steps.slice(0, index), step]; index++; }"
-      :forward="!backward"
-      :item="current.id"
-      :key="[current.id, !backward].join('_')"
-      ></Options>
+    <Breadcrumb v-if="data" @step-back="(i: number) => index = i" :start="data.start" :steps="steps" :end="data.end"
+      :forward="!backward" :key="[current.forward, data.start, ...steps, data.end].join('_')"></Breadcrumb>
+    <transition name="fade" mode="out-in">
+      <Options @step-advance="(step: Step) => { steps = [...steps.slice(0, index), step]; index++; }" :forward="!backward"
+        :item="current.id" :key="[current.id, !backward].join('_')"></Options>
+    </transition>
   </main>
 </template>
 
@@ -96,5 +82,15 @@ watch(current, (newCurrentValue, oldCurrentValue) => {
 main {
   display: grid;
   grid-template-rows: min-content auto;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
