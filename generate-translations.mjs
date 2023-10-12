81a8f694e2485f8b038078aa90d688c7771e3143
Théo Boulaire <th.boulaire@gmail.com>
2023-10-12 08:36:07 +0200

    Combining DeepL and given translations

--- generate-translations.mjs
+++ generate-translations.mjs
@@ -11,25 +11,41 @@ const deeplAuthKey = process.env.DEEPL_AUTH_KEY;
 const enLocale = JSON.parse(fs.readFileSync("locales/en.json").toString());
 
 const flatEnLocale = Object.entries(flatten(enLocale));
+/** DeepL API is able to translate chunks of texts as large as 50 */
 const deeplMaxChunkSize = 50;
-const chunkedEntries = Array.from({
-  length: Math.ceil(flatEnLocale.length / deeplMaxChunkSize),
-}).map((v, i) =>
-  flatEnLocale.slice(i * deeplMaxChunkSize, (i + 1) * deeplMaxChunkSize)
-);
 
 for (const targetLocale of ["es", "ja", "ko"]) {
-  const localeObject = unflatten(
-    Object.fromEntries(
+  const targetLocaleFilePath = `locales/${targetLocale}.json`;
+  const flatTargetLocale = (() => {
+    try {
+      return flatten(
+        JSON.parse(fs.readFileSync(targetLocaleFilePath).toString())
+      );
+    } catch (e) {
+      return {};
+    }
+  })();
+  const enEntriesToTranslate = flatEnLocale.filter(
+    ([key]) => !Object.hasOwn(flatTargetLocale, key)
+  );
+  const chunkedEnEntriesToTranslate = Array.from({
+    length: Math.ceil(enEntriesToTranslate.length / deeplMaxChunkSize),
+  }).map((v, i) =>
+    enEntriesToTranslate.slice(
+      i * deeplMaxChunkSize,
+      (i + 1) * deeplMaxChunkSize
+    )
+  );
+  const localeObject = unflatten({
+    ...Object.fromEntries(
       (
         await Promise.all(
-          chunkedEntries.map(async (entriesChunk) => {
+          chunkedEnEntriesToTranslate.map(async (chunk) => {
             const body = JSON.stringify({
-              text: entriesChunk.map(([, value]) => value), // 50 strings max
+              text: chunk.map(([, value]) => value), // 50 strings max
               target_lang: targetLocale,
-              // source_lang: 'en',
+              source_lang: "en",
             });
-            console.log(body);
             const response = await fetch(
               "https://api-free.deepl.com/v2/translate",
               {
@@ -42,43 +58,14 @@ for (const targetLocale of ["es", "ja", "ko"]) {
               }
             );
             const data = await response.json();
-            console.log(data);
-            return data.translations.map(({ text }, i) => [
-              entriesChunk[i][0],
-              text,
-            ]);
+            return data.translations.map(({ text }, i) => [chunk[i][0], text]);
           })
         )
       ).flat()
-    )
-  );
-  fs.writeFileSync(
-    `locales/${targetLocale}.json`,
-    JSON.stringify(localeObject)
-  );
+    ),
+    ...flatTargetLocale,
+  });
+  fs.writeFileSync(targetLocaleFilePath, JSON.stringify(localeObject));
 }
 
 console.log("generate-translations end");
-
-// fetch("https://api-free.deepl.com/v2/translate", {
-//   method: "POST",
-//   headers: new Headers({
-//     Authorization: `DeepL-Auth-Key ${deeplAuthKey}`,
-//     "Content-Type": "application/json",
-//   }),
-//   body: JSON.stringify({
-//     text: ["Hello, world!", "Yes"], // 50 strings max
-//     target_lang: "de",
-//   }),
-// })
-//   .then((response) => response.json())
-//   .then((data) => {
-
-//     {
-//       translations: [
-//         { detected_source_language: 'EN', text: 'Hallo, Welt!' },
-//         { detected_source_language: 'EN', text: 'Ja' }
-//       ]
-//     }
-//     console.log(data);
-//   });
