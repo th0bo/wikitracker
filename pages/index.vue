6e265edcb0324aa7f1219f720965a7bd63d4ce95
Théo Boulaire <th.boulaire@gmail.com>
2023-10-30 21:42:14 +0100

    Added sample game link to index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,11 @@
+<script setup lang="ts">
+const localePath = useLocalePath();
+</script>
+
 <template>
   <h1>{{ $t('index') }}</h1>
-  <!-- <NuxtLinkLocale to="/game/Q161904-Q41269">{{ $t('sharePath') }}</NuxtLinkLocale> -->
+  <ClientOnly>
+    <NuxtLink :to="localePath({ path: '/game/Q161904-Q41269' })">{{ $t('play') }}
+    </NuxtLink>
+  </ClientOnly>
 </template>
