ca9f55edf34908548e1c5865d32bc9a702cad9ba
Théo Boulaire <th.boulaire@gmail.com>
2023-11-09 16:36:59 +0100

    Enhanced version of the animated dice button component

--- components/DiceButton.vue
+++ components/DiceButton.vue
@@ -4,28 +4,10 @@
       <path class="frame" d="M 100 0 l 100 100 l -100 100 l -100 -100 Z M 100 10 l 90 90 l -90 90 l -90 -90 Z"></path>
       <g>
         <rect x="90" y="90" width="20" height="20"></rect>
-      </g>
-      <g>
-        <rect x="60" y="60" width="20" height="20"></rect>
-        <rect x="120" y="120" width="20" height="20"></rect>
-      </g>
-      <g>
-        <rect x="60" y="60" width="20" height="20"></rect>
         <rect x="90" y="90" width="20" height="20"></rect>
-        <rect x="120" y="120" width="20" height="20"></rect>
-      </g>
-      <g>
-        <rect x="60" y="60" width="20" height="20"></rect>
-        <rect x="120" y="120" width="20" height="20"></rect>
-        <rect x="120" y="60" width="20" height="20"></rect>
-        <rect x="60" y="120" width="20" height="20"></rect>
-      </g>
-      <g>
-        <rect x="60" y="60" width="20" height="20"></rect>
         <rect x="90" y="90" width="20" height="20"></rect>
-        <rect x="120" y="120" width="20" height="20"></rect>
-        <rect x="60" y="120" width="20" height="20"></rect>
-        <rect x="120" y="60" width="20" height="20"></rect>
+        <rect x="90" y="90" width="20" height="20"></rect>
+        <rect x="90" y="90" width="20" height="20"></rect>
       </g>
     </svg>
   </button>
@@ -53,7 +35,6 @@ svg {
 path {
   fill: var(--blue);
   transform-origin: center;
-  transition: translate 0.2s ease-in-out;
   fill-rule: evenodd;
 }
 
@@ -63,44 +44,379 @@ rect {
   rotate: 0.125turn;
 }
 
-@keyframes face {
+@keyframes dot1 {
   0% {
-    opacity: 0;
+    x: 90;
+    y: 90;
   }
-  4% {
-    opacity: 1;
+  10% {
+    x: 90;
+    y: 90;
   }
-  16% {
-    opacity: 1;
+  12.5% {
+    x: 60;
+    y: 60;
   }
-  20% {
-    opacity: 0;
+  22.5% {
+    x: 60;
+    y: 60;
+  }
+  25% {
+    x: 90;
+    y: 90;
+  }
+  35% {
+    x: 90;
+    y: 90;
+  }
+  37.5% {
+    x: 60;
+    y: 60;
+  }
+  47.5% {
+    x: 60;
+    y: 60;
+  }
+  50% {
+    x: 90;
+    y: 90;
+  }
+  60% {
+    x: 90;
+    y: 90;
+  }
+  62.5% {
+    x: 60;
+    y: 60;
+  }
+  72.5% {
+    x: 60;
+    y: 60;
+  }
+  75% {
+    x: 90;
+    y: 90;
+  }
+  85% {
+    x: 90;
+    y: 90;
+  }
+  87.5% {
+    x: 60;
+    y: 60;
+  }
+  97.5% {
+    x: 60;
+    y: 60;
   }
   100% {
-    opacity: 0;
+    x: 90;
+    y: 90;
   }
 }
 
-g {
-  animation-name: face;
-  animation-duration: 5s;
+@keyframes dot2 {
+  0% {
+    x: 90;
+    y: 90;
+  }
+  10% {
+    x: 90;
+    y: 90;
+  }
+  12.5% {
+    x: 60;
+    y: 60;
+  }
+  22.5% {
+    x: 60;
+    y: 60;
+  }
+  25% {
+    x: 90;
+    y: 90;
+  }
+  35% {
+    x: 90;
+    y: 90;
+  }
+  37.5% {
+    x: 60;
+    y: 60;
+  }
+  47.5% {
+    x: 60;
+    y: 60;
+  }
+  50% {
+    x: 90;
+    y: 90;
+  }
+  60% {
+    x: 90;
+    y: 90;
+  }
+  62.5% {
+    x: 60;
+    y: 60;
+  }
+  72.5% {
+    x: 60;
+    y: 60;
+  }
+  75% {
+    x: 90;
+    y: 90;
+  }
+  85% {
+    x: 90;
+    y: 90;
+  }
+  87.5% {
+    x: 60;
+    y: 120;
+  }
+  97.5% {
+    x: 60;
+    y: 120;
+  }
+  100% {
+    x: 90;
+    y: 90;
+  }
+}
+
+@keyframes dot3 {
+  0% {
+    x: 90;
+    y: 90;
+  }
+  10% {
+    x: 90;
+    y: 90;
+  }
+  12.5% {
+    x: 60;
+    y: 60;
+  }
+  22.5% {
+    x: 60;
+    y: 60;
+  }
+  25% {
+    x: 90;
+    y: 90;
+  }
+  35% {
+    x: 90;
+    y: 90;
+  }
+  37.5% {
+    x: 60;
+    y: 60;
+  }
+  47.5% {
+    x: 60;
+    y: 60;
+  }
+  50% {
+    x: 90;
+    y: 90;
+  }
+  60% {
+    x: 90;
+    y: 90;
+  }
+  62.5% {
+    x: 60;
+    y: 120;
+  }
+  72.5% {
+    x: 60;
+    y: 120;
+  }
+  75% {
+    x: 90;
+    y: 90;
+  }
+  85% {
+    x: 90;
+    y: 90;
+  }
+  87.5% {
+    x: 120;
+    y: 120;
+  }
+  97.5% {
+    x: 120;
+    y: 120;
+  }
+  100% {
+    x: 90;
+    y: 90;
+  }
+}
+
+@keyframes dot4 {
+  0% {
+    x: 90;
+    y: 90;
+  }
+  10% {
+    x: 90;
+    y: 90;
+  }
+  12.5% {
+    x: 60;
+    y: 60;
+  }
+  22.5% {
+    x: 60;
+    y: 60;
+  }
+  25% {
+    x: 90;
+    y: 90;
+  }
+  35% {
+    x: 90;
+    y: 90;
+  }
+  37.5% {
+    x: 90;
+    y: 90;
+  }
+  47.5% {
+    x: 90;
+    y: 90;
+  }
+  50% {
+    x: 90;
+    y: 90;
+  }
+  60% {
+    x: 90;
+    y: 90;
+  }
+  62.5% {
+    x: 120;
+    y: 60;
+  }
+  72.5% {
+    x: 120;
+    y: 60;
+  }
+  75% {
+    x: 90;
+    y: 90;
+  }
+  85% {
+    x: 90;
+    y: 90;
+  }
+  87.5% {
+    x: 120;
+    y: 60;
+  }
+  97.5% {
+    x: 120;
+    y: 60;
+  }
+  100% {
+    x: 90;
+    y: 90;
+  }
+}
+
+@keyframes dot5 {
+  0% {
+    x: 90;
+    y: 90;
+  }
+  10% {
+    x: 90;
+    y: 90;
+  }
+  12.5% {
+    x: 120;
+    y: 120;
+  }
+  22.5% {
+    x: 120;
+    y: 120;
+  }
+  25% {
+    x: 90;
+    y: 90;
+  }
+  35% {
+    x: 90;
+    y: 90;
+  }
+  37.5% {
+    x: 120;
+    y: 120;
+  }
+  47.5% {
+    x: 120;
+    y: 120;
+  }
+  50% {
+    x: 90;
+    y: 90;
+  }
+  60% {
+    x: 90;
+    y: 90;
+  }
+  62.5% {
+    x: 120;
+    y: 120;
+  }
+  72.5% {
+    x: 120;
+    y: 120;
+  }
+  75% {
+    x: 90;
+    y: 90;
+  }
+  85% {
+    x: 90;
+    y: 90;
+  }
+  87.5% {
+    x: 90;
+    y: 90;
+  }
+  97.5% {
+    x: 90;
+    y: 90;
+  }
+  100% {
+    x: 90;
+    y: 90;
+  }
+}
+
+rect {
+  animation-duration: 10s;
   animation-iteration-count: infinite;
   animation-direction: forwards;
 }
-
-g:nth-child(1) {
-  animation-delay: 0s;
+rect:nth-child(1) {
+  animation-name: dot1;
 }
-g:nth-child(2) {
-  animation-delay: 1s;
+rect:nth-child(2) {
+  animation-name: dot2;
 }
-g:nth-child(3) {
-  animation-delay: 2s;
+rect:nth-child(3) {
+  animation-name: dot3;
 }
-g:nth-child(4) {
-  animation-delay: 3s;
+rect:nth-child(4) {
+  animation-name: dot4;
 }
-g:nth-child(5) {
-  animation-delay: 4s;
+rect:nth-child(5) {
+  animation-name: dot5;
 }
 </style>
