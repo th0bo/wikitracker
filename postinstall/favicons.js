bb79947e179a098367709971a4c2bee6e8a17cd0
Théo Boulaire <th.boulaire@gmail.com>
2023-12-08 10:05:33 +0100

    Postinstall scripts organization

--- postinstall/favicons.js
+++ postinstall/favicons.js
@@ -5,17 +5,17 @@ const { convertFile } = require("convert-svg-to-png");
 
 console.log('generate-favicons start');
 
-const publicFolderPath = path.join(__dirname, 'public');
+const publicFolderPath = "public";
 if (!fs.existsSync(publicFolderPath)) {
   fs.mkdirSync(publicFolderPath);
 }
-const faviconFolderPath = path.join(__dirname, "public", "favicon");
+const faviconFolderPath = path.join("public", "favicon");
 if (!fs.existsSync(faviconFolderPath)) {
   fs.mkdirSync(faviconFolderPath);
 }
 
 (async () => {
-  const inputFilePath = path.join(__dirname, "assets", "svg", "favicon.svg");
+  const inputFilePath = path.join("assets", "svg", "favicon.svg");
   await Promise.all([16, 32, 48].map(size => {
     const outputFilePath = `${faviconFolderPath}/${size}x${size}.png`;
     return convertFile(inputFilePath, { outputFilePath, width: size, height: size });
